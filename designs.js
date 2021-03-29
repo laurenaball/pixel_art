12e28091fd9da28ccc06091bd860a11ce8f516b1
Lauren Ball <lball153@gmail.com>
2021-03-29 16:06:22 -0400

    Need help with pixel art maker

---
+++ designs.js
@@ -0,0 +1,25 @@
+// Select color input
+var color = document.getElementById('colorPicker');
+// Select size input
+const columns = document.getElementById('inputHeight').value;
+const rows = document.getElementById('inputWidth').value;
+
+// When size is submitted by the user, call makeGrid()
+var form = document.getElementById('sizePicker');
+form.addEventListener('submit', makeGrid);
+
+
+function makeGrid() {
+  var table = document.createElement('table')
+  for (var i = 0; i<columns; i++){
+    table.insertRow();
+    for (var j = 0; j<rows; j++){
+      var cell = celltable.insertcell();
+      cell.addEventListener('click', colorSquare(cell))
+    }
+  }
+}
+
+function colorSquare(cell) {
+  cell.style.backgroundColor = color.value;
+}
