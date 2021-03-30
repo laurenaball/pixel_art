f8b3df3d55f0b09aeb1ec07ca7c326774d7491ca
Lauren Ball <lball153@gmail.com>
2021-03-30 15:39:04 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -9,10 +9,7 @@ var form = document.getElementById('sizePicker');
 form.addEventListener('submit', function(event){
 event.preventDefault();
 makeGrid(rows, columns)
-});//?? To clarify, so now I am still running addEventListener on form and
-//passing (event = 'submit', function(event)) but now the
-//function is a new function that 1. prevents the default and then
-//2. calls the makeGrid function (passing it the rows and columns values)
+});
 
 function makeGrid(height, width) {
   var table = document.getElementById('pixelCanvas');
@@ -20,13 +17,8 @@ function makeGrid(height, width) {
     var row = table.insertRow();
     for (var j = 0; j<width; j++){
       var cell = row.insertCell();
-      cell.addEventListener('click', colorSquare(cell))//is this right syntax?
-    }//It doesn't matter if I write as below, right?:
-    //cell.addEventListener('click', function(cell){
-    //cell.style.backgroundColor = color.value;}
+      cell.addEventListener('click', function(event){
+        event.target.style.backgroundColor = color.value;})
+    }
   }
-}
-
-function colorSquare(cell) {
-  cell.style.backgroundColor = color.value;
 }
