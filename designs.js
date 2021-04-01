805b99cf4d2dcf1fbba93b032231dd7137dea7c5
Lauren Ball <lball153@gmail.com>
2021-04-01 14:46:40 -0400

    Update
    need help with clearing table

--- designs.js
+++ designs.js
@@ -1,16 +1,23 @@
 // Select color input
-var color = document.getElementById('colorPicker');
+const color = document.getElementById('colorPicker');
 // Select size input
-const rows = document.getElementById('inputHeight').value;
-const columns = document.getElementById('inputWidth').value;
+const height = document.getElementById('inputHeight');
+const width = document.getElementById('inputWidth');
 
-// When size is submitted by the user, call makeGrid()
+
+// When size is submitted by the user, call makeGrid(), set rows and columns
+// to input values
 var form = document.getElementById('sizePicker');
 form.addEventListener('submit', function(event){
-event.preventDefault();
-makeGrid(rows, columns)
+  event.preventDefault();
+  const rows = height.value;
+  const columns = width.value;
+  makeGrid(rows, columns)
 });
 
+// Use input values to create rows for the heights and add width number
+// of cells to each row. Adds event listener to each cell to color when 
+// clicked
 function makeGrid(height, width) {
   var table = document.getElementById('pixelCanvas');
   for (var i = 0; i<height; i++){
